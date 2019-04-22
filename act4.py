777a4cfaf7f45b11b6d146bb7f0a0cfb012cf732
Federico Mione <fedemione@gmail.com>
2019-04-22 16:15:43 -0300

    Random agent - act4

--- act4.py
+++ act4.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 
 # posibles estados del agente
@@ -22,7 +23,7 @@ class Agent:
         self.state = State.BOTTOM
 
         # recompenza acumulada
-        self.reward = 0
+        self.accumulated_reward = 0
 
         # define el entorno (current state, action) => (next state, reward)
         self.environment = {
@@ -40,26 +41,42 @@ class Agent:
             }
         }
 
-        # self.environment = {
-        #     (State.BOTTOM, Action.REST): (State.BOTTOM, 0.1),
-        #     (State.BOTTOM, Action.CLIMB): (State.TOP, 0.0),
-        #     (State.MIDDLE, Action.CLIMB): (State.TOP, 0.3),
-        #     (State.MIDDLE, Action.DOWN): (State.BOTTOM, 1.0),
-        #     (State.TOP, Action.REST): (State.TOP, 0.1),
-        #     (State.TOP, Action.DOWN): (State.MIDDLE, 0.2),
-        # }
+        # muestra el estado actual
+        print("Estado inicial: BOTTOM")
+        print("Recompenza acumulada: 0")
 
-    # def get_posible_actions(self, action):
+    # función que retorna las posibles acciones del estado actual
+    def get_possible_action(self):
+        return list(self.environment[self.state].keys())
 
+    # función que retorna las posibles acciones del estado actual y la recompenza de cada una
+    def get_possible_action_reward(self):
+        return list(self.environment[self.state].items())
+
+    # función que permite al agente moverse en un entorno definido (realizar una accion según su estado actual)
     def move(self, action):
         # controla que la accion se pueda ejecutar en el estado actual
         if action in list(self.environment[self.state].keys()):
-            print(True)
-        else:
-            print(False)
-            # self.reward += self.environment[(State.BOTTOM, Action.REST)]
-        # print(action in list(self.environment[self.state].keys()))
+            self.accumulated_reward += self.environment[self.state][action][1]
+            self.state = self.environment[self.state][action][0]
 
+            print("Acción: {}".format(action))
+            print("Estado: {}".format(self.state))
+            print("Recompenza acumulada: %.2f" % self.accumulated_reward)
+        else:
+            print("Movimiento no permitido - Existe un error")
+
+
+def random_agent():
+    # crea el agente
+    agent = Agent()
+
+    for i in range(5):
+        actions = agent.get_possible_action()
+        random_action = int(np.random.rand() * 2)
+        agent.move(actions[random_action])
+
+random_agent()
 
 agent = Agent()
-agent.move(Action.REST)
+print(agent.get_possible_action_reward())
